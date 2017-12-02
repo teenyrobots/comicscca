d2d7f123634edbed9dbcc8122598cffb8ddbe08b
Kevin Moore <k.james.moore@gmail.com>
2017-12-01 21:17:07 -0500

    directory displaying students

--- students.js
+++ students.js
@@ -1,31 +1,4 @@
 // This is a JS data file we import into other pages
 
-var students_2015 = [
-    {
-        displayName: 'Guillory Martin',
-        slug: "guillory",
-        year: '2015',
-        thesisPages: 30,
-        spread: true,
-        thesisTitle: "Sam's Town",
-        digitalTitle: "Morning Melody"
-    },
-    {
-        displayName: 'Kelly Jones',
-        slug: "kelly",
-        year: '2015',
-        thesisPages: 8,
-        spread: false,
-        thesisTitle: "Shorts",
-        digitalTitle: "How To Catch A Spider"
-    },
-    {
-        displayName: 'Kobabe Bryant',
-        year: '2015',
-        thesisPages: 20,
-        spread: false,
-        thesisTitle: "Sam's Town",
-        digitalTitle: "Morning Melody"
-    }
-];
+
 
