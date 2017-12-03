ffbe0a83c9fa42c46441d7709caa97af9dc7563e
Joyce Rice <joycer@joyces-mbp.home>
2017-12-03 15:32:46 -0500

    starts to convert background image code to using students.json instead of case statment

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function randomBackground() {
-    var backgroundNum = Math.floor(Math.random() * 15) + 1;
-    var backgroundImg = "images/backgrounds/" + backgroundNum + ".jpg";
-    $('body').css('background-image', 'url(' + backgroundImg + ')');
-    whichStudent(backgroundNum);
-};
+
 
 function whichStudent(x) {
     var student = {}
@@ -154,6 +149,6 @@ function whichStudent(x) {
             break;
     };
 
-    $("#name").append(student.name);
-    $("#title").append(student.title);
+    $("#footerName").append(student.name);
+    $("#footerTitle").append(student.title);
 }
